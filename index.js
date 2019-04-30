d4e9794698bfb3caa9aaf9d43233017235f20582
Tera Coder <TerableCoder@gmail.com>
2019-04-30 03:03:11 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function sign(dispatch) {
-	mod.game.initialize(["me"]);
+	dispatch.game.initialize(["me"]);
 	dispatch.hook('S_AIR_REACTION_END', 1, (event) => {
-		if(mod.game.me.is(event.gameId)) {
+		if(dispatch.game.me.is(event.gameId)) {
 			event.loc.z += 4;
 			return true;
 		}
